a41766d90017f67ff9b8e2edefbc36d8f3254b05
Handika Rafli A <shandika231@gmail.com>
2022-09-29 17:32:00 +0700

    project structure and add splash screen package

---
+++ my_doctor/lib/application.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_doctor/presentation/page/home_page.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 640),
+      splitScreenMode: true,
+      minTextAdapt: true,
+      builder: (context, child) {
+        print(child);
+        return MaterialApp(
+          title: 'My Doctor',
+          theme: ThemeData(
+            primarySwatch: Colors.cyan,
+          ),
+          home: const HomePage(),
+        );
+      },
+    );
+  }
+}
