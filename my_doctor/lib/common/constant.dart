ca3f8f6012ede78784cd05551552120c6b868b60
Handika Rafli A <shandika231@gmail.com>
2022-10-02 16:13:01 +0700

    add color

---
+++ my_doctor/lib/common/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  Color cyanColor = const Color(0xff0BCAD4);
+  Color whiteColor = const Color(0xffffffff);
+  Color blackColor = const Color(0xff112340);
+}
